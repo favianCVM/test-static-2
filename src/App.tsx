57c0c12016e0b9d370be6b9d7a84a230594a7089
fabian <koe7the@gmail.com>
2023-10-10 21:40:07 -0400

    dd

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,21 @@ import { Helmet, HelmetProvider } from 'react-helmet-async';
 function App() {
   return (<HelmetProvider>
     <Helmet>
+      <title>¡Tu amigo te ha invitado a zumma!</title>
+      <meta name="title" content="¡Tu amigo te ha invitado a zumma!" />
+      <meta name="description" content="Zumma es tu comunidad y plataforma de ahorro e inversión enfocada a enseñarte paso a paso cómo y en qué invertir y donde podrás seguir e interactuar con otros inversionistas como tú. Te ayudaremos a escoger en qué fondos de inversiones poner tu dinero a trabajar." />
+
       <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://metatags.io/" />
       <meta property="og:title" content="¡Tu amigo te ha invitado a zumma!" />
       <meta property="og:description" content="Zumma es tu comunidad y plataforma de ahorro e inversión enfocada a enseñarte paso a paso cómo y en qué invertir y donde podrás seguir e interactuar con otros inversionistas como tú. Te ayudaremos a escoger en qué fondos de inversiones poner tu dinero a trabajar." />
-      <meta name="image" property="og:image" content="https://png.pngtree.com/element_our/png/20181129/vector-illustration-of-fresh-red-apple-with-single-leaf-png_248312.jpg" />
+      <meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://metatags.io/" />
+      <meta property="twitter:title" content="¡Tu amigo te ha invitado a zumma!" />
+      <meta property="twitter:description" content="Zumma es tu comunidad y plataforma de ahorro e inversión enfocada a enseñarte paso a paso cómo y en qué invertir y donde podrás seguir e interactuar con otros inversionistas como tú. Te ayudaremos a escoger en qué fondos de inversiones poner tu dinero a trabajar." />
+      <meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
     </Helmet>
     <Router />
   </HelmetProvider>);
