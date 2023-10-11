82823fd456925e73460500fbd7ee93696f715786
fabian <koe7the@gmail.com>
2023-10-10 20:41:24 -0400

    dd

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import {Router} from "./components/routing/Router";
+import {Helmet} from 'react-helmet';
 
 function App() {
-  return (<Router />);
+  return (<>
+    <Helmet>
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="¡Tu amigo te ha invitado a zumma!" />
+      <meta property="og:description" content="Zumma es tu comunidad y plataforma de ahorro e inversión enfocada a enseñarte paso a paso cómo y en qué invertir y donde podrás seguir e interactuar con otros inversionistas como tú. Te ayudaremos a escoger en qué fondos de inversiones poner tu dinero a trabajar." />
+      <meta name="image" property="og:image" content="https://files.slack.com/files-tmb/T03PBQZB144-F060B2GM68M-b0c0bd86d2/preview_480.png" />
+    </Helmet>
+    <Router />
+  </>);
 }
 
 export default App;
--- src/components/routing/Router.tsx
+++ src/components/routing/Router.tsx
@@ -5,7 +5,6 @@ import {
 import { Layout } from "../Layout/Layout";
 import {HomePage} from '../../pages/HomePage';
 import { Status404 } from "../../pages/404";
-import {Helmet} from "react-helmet";
 
 const router = createBrowserRouter([
   {
@@ -21,14 +20,6 @@ const router = createBrowserRouter([
 const Router = () => {
   return(
     <Layout>
-      {/* @ts-ignore */}
-      <Helmet>
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="¡Tu amigo te ha invitado a zumma!" />
-        <meta property="og:description" content="Zumma es tu comunidad y plataforma de ahorro e inversión enfocada a enseñarte paso a paso cómo y en qué invertir y donde podrás seguir e interactuar con otros inversionistas como tú. Te ayudaremos a escoger en qué fondos de inversiones poner tu dinero a trabajar." />
-        <meta name="image" property="og:image" content="https://files.slack.com/files-tmb/T03PBQZB144-F060B2GM68M-b0c0bd86d2/preview_480.png" />
-      </Helmet>
-
       <RouterProvider router={router} />
     </Layout>
   )
