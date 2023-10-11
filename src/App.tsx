517ce5b10e0839b5c060e469c803878a7a7daa53
fabian <koe7the@gmail.com>
2023-10-10 21:34:23 -0400

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <meta property="og:type" content="website" />
       <meta property="og:title" content="¡Tu amigo te ha invitado a zumma!" />
       <meta property="og:description" content="Zumma es tu comunidad y plataforma de ahorro e inversión enfocada a enseñarte paso a paso cómo y en qué invertir y donde podrás seguir e interactuar con otros inversionistas como tú. Te ayudaremos a escoger en qué fondos de inversiones poner tu dinero a trabajar." />
-      <meta name="image" property="og:image" content="https://files.slack.com/files-tmb/T03PBQZB144-F060B2GM68M-b0c0bd86d2/preview_480.png" />
+      <meta name="image" property="og:image" content="https://png.pngtree.com/element_our/png/20181129/vector-illustration-of-fresh-red-apple-with-single-leaf-png_248312.jpg" />
     </Helmet>
     <Router />
   </HelmetProvider>);
